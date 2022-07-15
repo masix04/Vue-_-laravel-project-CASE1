8dc819a5648f3f292a2c65204f1ab764e8ea4067
muhammad.ahmed <muhammad.ahmed@sixlogics.com>
2022-07-15 18:37:18 +0500

    Updated Player related APIs

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Http\Request;
 use App\Models\Team;
+use App\Models\Player;
 use App\Models\PlayerTeam;
 use App\Models\TournamentTeam;
 use DB;
@@ -22,12 +23,13 @@ class Controller extends BaseController
         $data['code'] = 200;
         $data['message'] = 'success';
 
-        $data['data']['duplicate_players']['count'] = PlayerTeam::selectRaw('count(player_id) as players_count')->groupBy('player_id')->having('players_count','>',2)->get();
-        $data['data']['duplicate_players']['player_ids'] = PlayerTeam::select('player_id')->groupBy('player_id')->havingRaw('count(player_id) > 2')->get();
+        $data['data']['duplicate_players']['count'] = PlayerTeam::selectRaw('count(player_id) as players_count')->groupBy('player_id','team_id')->having('players_count','>',1)->get();
+        $data['data']['duplicate_players']['player_ids'] = PlayerTeam::select('team_id','player_id')->groupBy('player_id','team_id')->havingRaw('count(player_id) > 1')->get();
         $data['data']['duplicate_players']['players'] = PlayerTeam::with(['players'=> function($q) {
                                         $q->select('id','slug','name');
                                     }])
-                                    ->whereIn('player_id',$data['data']['duplicate_players']['player_ids'])->select('id as player_team_id','player_id','team_id')->orderBy('player_id')->limit(10)->get();
+                                    ->whereIn('player_id',$data['data']['duplicate_players']['player_ids'])
+                                    ->select('id as player_team_id','player_id','team_id')->orderBy('player_id')->orderBy('team_id')->limit(100)->get();
 
         return response()->json($data);
     }
@@ -37,12 +39,12 @@ class Controller extends BaseController
         $data['title'] = 'Delete Duplicate PLAYER';
         $data['code'] = 200;
         $data['message'] = 'success';
-
+        $data['data'] = $request->player_team_id;
         // $DeleteCheck = PlayerTeam::where('id',$request->player_team_id)->delete();
         $data['deleted'] = DB::delete("DELETE from `player_team` where `id` = '".$request->player_team_id."'");
-        $data['save'] = PlayerTeam::create(['id' => 199997, 'player_id' => 468, 'team_id' => 48])->save();
+        // $data['save'] = PlayerTeam::create(['id' => 199997, 'player_id' => 468, 'team_id' => 48])->save();
 
-        $data['save'] = (($data['save'] == 1)? TRUE : FALSE);
+        // $data['save'] = (($data['save'] == 1)? TRUE : FALSE);
         $data['deleted'] = (($data['deleted'] == 1)? TRUE : FALSE);
 
         // PlayerTeam::where('player_id',$request->player_id)->where('team_id',$request->team_id)->delete();
@@ -58,7 +60,8 @@ class Controller extends BaseController
         $data['code'] = 200;
         $data['message'] = 'success';
 
-        $data['updated'] = PlayerTeam::where('id',200000)->update(['player_id' => 469, 'team_id' => 50])->save();
+        // $data['updated'] = PlayerTeam::where('id',$request->team_id)->update(['name' => $request->name, 'slug' => $request->slug])->save();
+        $data['updated'] = Player::where('id',$request->player_id)->update(['name' => $request->name, 'slug' => $request->slug]);
 
         return response()->json($data);
 
--- database/sql_queries/Anomalies_remove_from_player_team_KISMA_Table.sql
+++ database/sql_queries/Anomalies_remove_from_player_team_KISMA_Table.sql
@@ -1,6 +1,10 @@
 -- brought count for more than 1 which has 
-SELECT COUNT(pt.player_id), pt.`player_id` FROM player_team pt 
+SELECT COUNT(pt.player_id), pt.`team_id`, pt.`player_id` FROM player_team pt 
 GROUP BY pt.`player_id` HAVING COUNT(pt.`player_id`) > 1
+
+-- Updated [more accurate & ordered So easy Deletion]
+SELECT COUNT(pt.player_id),pt.`team_id`, pt.`player_id` FROM player_team pt 
+GROUP BY pt.`player_id`,pt.`team_id` HAVING COUNT(pt.`player_id`) > 1
 
 -- Brought Player_ids for count more than 1
 SELECT  pt.`player_id` FROM player_team pt 
